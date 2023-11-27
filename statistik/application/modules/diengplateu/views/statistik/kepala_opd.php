095f3ba7ed8a5f47fa8723a6b5590e2e6497cd76
priyo <rawazhia@gmail.com>
2023-11-27 10:07:09 +0700

    tes

--- statistik/application/modules/diengplateu/views/statistik/kepala_opd.php
+++ statistik/application/modules/diengplateu/views/statistik/kepala_opd.php
@@ -6,7 +6,7 @@
 </div>
 
 <div class="row">
- <a href="<?=base_URL()?>diengplateu/page/download_excell">
+ <a href="<?=base_URL()?>diengplateu/page/download_excell" target="_blank">
 <button type="submit" class="btn btn-sm btn-primary btn-sidebar">Download</button></a>
 </form>
 </div>
