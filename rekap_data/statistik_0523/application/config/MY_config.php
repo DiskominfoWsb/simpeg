df5182e1b16280c0a797d74a36478bbf7d8dec01
danu4321 <danucp@gmail.com>
2024-01-11 16:17:08 +0700

    Menambah folder statistik_0523

---
+++ rekap_data/statistik_0523/application/config/MY_config.php
@@ -0,0 +1,21 @@
+<?php
+
+$config['hari']		= array("","Minggu","Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu");
+$config['bulan']	= array("","Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
+$config['data']['hari'] 	= $config['hari'];
+$config['data']['bulan'] 	= $config['bulan'];
+
+$config['data']['site_title']  		= 'Data Statistik PNS Pemkab. Wonosobo';
+$config['data']['site_heading']		= 'Data Statistik PNS Pemkab. Wonosobo';
+$config['data']['site_copyright']	= 'Copyright &copy; '.date('Y').' Sie BKD Wonosobo. Developed by Dinustek.';
+$config['data']['site_heading1']    = 'Data Statistik PNS Pemkab. Wonosobo';
+$config['data']['site_desc']		= 'Data Statistik PNS Pemkab. Wonosobo';
+$config['data']['themes']			= 'diengplateu/diengplateu/';
+
+$config['base_url']					= 'https://'.$_SERVER['SERVER_NAME'].'/statistik/';
+
+$config['data']['def_images']		= $config['base_url'].'assets/images/';
+$config['data']['def_img']			= $config['base_url'].'assets/img/';
+$config['data']['def_ico']			= $config['base_url'].'assets/ico/';
+$config['data']['def_css']			= $config['base_url'].'assets/css/';
+$config['data']['def_js']			= $config['base_url'].'assets/js/';
