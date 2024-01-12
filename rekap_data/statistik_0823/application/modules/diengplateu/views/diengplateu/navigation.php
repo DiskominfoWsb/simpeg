a6da232dd8e8428667e5dfc24b0f329c579a405f
danu4321 <danucp@gmail.com>
2024-01-12 15:09:40 +0700

    edit navigation.php

--- rekap_data/statistik_0823/application/modules/diengplateu/views/diengplateu/navigation.php
+++ rekap_data/statistik_0823/application/modules/diengplateu/views/diengplateu/navigation.php
@@ -146,8 +146,10 @@
                         </li>
                         <li class="sidebar-search">
                             <footer>
-                                &copy;2014 Kab. Wonosobo.<br>
-                                Dikembangkan oleh BKD</a>.
+                                Copyright &copy; <?php
+  $fromYear = 2014; 
+  $thisYear = (int)date('Y'); 
+  echo $fromYear . (($fromYear != $thisYear) ? '-' . $thisYear : '');?> BKD Kab. Wonosobo
                             </footer>
                         </li>
                     </ul>
