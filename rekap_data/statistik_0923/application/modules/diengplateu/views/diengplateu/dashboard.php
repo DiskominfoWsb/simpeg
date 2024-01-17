05de6d0c91f8118b63c22d406b1c7b7a10459eae
danu4321 <danucp@gmail.com>
2024-01-17 16:37:47 +0700

    ubah dashboard

--- rekap_data/statistik_0923/application/modules/diengplateu/views/diengplateu/dashboard.php
+++ rekap_data/statistik_0923/application/modules/diengplateu/views/diengplateu/dashboard.php
@@ -355,7 +355,7 @@
 
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Dashboard</h1>
+                    <h1 class="page-header">Data Bulan September 2023</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
